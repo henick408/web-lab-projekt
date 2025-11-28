ce9fc8df9adf7947d59e06fa00f923f92e163df0
henick408 <marcel.snopkowski@gmail.com>
2025-11-28 10:08:16 +0100

    Backend initial commit

---
+++ backend/src/main/kotlin/com/henick/web_lab_projekt_backend/WebLabProjektBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.henick.web_lab_projekt_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebLabProjektBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebLabProjektBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/henick/web_lab_projekt_backend/WebLabProjektBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.henick.web_lab_projekt_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebLabProjektBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
